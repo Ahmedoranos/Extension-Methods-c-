83c0dc44934dad2e0d75a77f88c8d399e8d01451
Ahmed Ashraf <36471213+Ahmedoranos@users.noreply.github.com>
2023-03-08 20:14:34 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-
+using System;					
+public class Program
+{
+	public static void Main()
+	{
+		string str = "ahmed";
+		string method_result = str.ChangeFirstLetterCase();
+	}
+}
+public static class StringHelper
+{
+	public static string ChangeFirstLetterCase(this string inputString)
+	{
+		if (inputString.Length > 0)
+        {
+            char[] charArray = inputString.ToCharArray();
+            charArray[0] = char.IsUpper(charArray[0]) ?
+                char.ToLower(charArray[0]) : char.ToUpper(charArray[0]);
+            return new string(charArray);
+        }
+        return inputString;
+	}
+}
